a3a1ae64d973c40452bb43849b0123935a21206b
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2023-10-19 22:14:46 +0300

    Address model was created

---
+++ src/main/models/Address.kt
@@ -0,0 +1,24 @@
+import java.lang.Exception
+
+interface IAddress{
+    val street: String
+    val city: String
+    //val postalCode: String
+    //Additional details => mb add
+}
+
+class Address(_street: String, _city: String) : IAddress{
+    override var street: String
+        private set
+    override var city: String
+        private set
+
+    init {
+
+        if (_street.isEmpty() || _city.isEmpty())
+            throw Exception("Incorrect data")
+
+        street = _street
+        city = _city
+    }
+}
